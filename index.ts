5243ba975c804478865120217e8d3135ab524748
sKawashima <bysk.333@gmail.com>
2019-09-17 16:09:42 +0900

    Change fieldAngle, Add filename setting

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import * as capture from 'capture-chrome'
 import { writeFileSync } from 'fs'
 
-export const squareCapture = (url:string) => {
+export const squareCapture = (url:string, fileName?: string) => {
   capture({
     url: url,
-    width: 1024,
-    height: 1024
+    // 16:9
+    width: 1200,
+    height: 675
   }).then(screenshot => {
-    const fileName = `${__dirname}/${Date.now()}.png`
-    writeFileSync(fileName, screenshot)
-    console.log(fileName)
+    const outputFileName = fileName ? `${__dirname}/${fileName}.png` : `${__dirname}/${Date.now()}.png`
+    writeFileSync(outputFileName, screenshot)
+    console.log(outputFileName)
   })
 }
 
 // test
-squareCapture('https://www.mwed.jp/')
+squareCapture('https://www.hanamaruudon.com/', 'hanamaruudon.com')
+squareCapture('https://www.marugame-seimen.com', 'marugame-seimen.com')
+squareCapture('https://www.gyomusuper.jp/', 'gyomusuper.jp')
